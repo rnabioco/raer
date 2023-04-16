09a05ff9f80828dee3fe492cc9a14feeadd3c043
Kent Riemondy <kent.riemondy@gmail.com>
2023-04-16 10:23:39 -0600

    use BiocFileCache for vignette data

--- R/cache.R
+++ R/cache.R
@@ -1,8 +1,9 @@
 #' Download GSE99249 BAM files and related data
 #'
-#' @description  This function will download ~ 250 MB of data.
+#' @description  This function will download ~ 250 MB of data and store files in a
+#' BiocFileCache.
 #'
-#' @param path path to directory to download data
+#' @param verbose report messages
 #' @returns A named list with paths to BAM files, a FASTA file and a bed
 #' file of known editing sites from hg38 chromosome 18.
 #'
@@ -10,14 +11,13 @@
 #'
 #' @examples
 #' \donttest{
-#' td <- tempdir()
-#' download_GSE99249(td)
+#' download_GSE99249()
 #' }
 #'
+#' @importFrom utils relist
 #' @export
-download_GSE99249 <- function(path) {
-    dir.create(path, recursive = TRUE, showWarnings = FALSE)
-    options(timeout = max(1000, getOption("timeout")))
+download_GSE99249 <- function(verbose = TRUE) {
+
     baseURL <- "https://raer-test-data.s3.us-west-2.amazonaws.com/GSE99249/"
 
     bam_fns <- c(
@@ -35,28 +35,18 @@ download_GSE99249 <- function(path) {
         bed = "rediportal_hg38_chr18.bed.gz"
     )
 
-    fids <- list()
-    for (i in seq_along(GSE99249_files)) {
-        fns <- GSE99249_files[[i]]
-        ftype <- names(GSE99249_files)[i]
-        out_fns <- unlist(lapply(fns, function(x) {
-            fn <- file.path(path, x)
-            if (!file.exists(fn)) {
-                # wb  necessary to avoid windows mangling line endings...
-                download.file(paste0(baseURL, x), fn, mode = "wb")
-            }
-            fn
-        }))
-        names(out_fns) <- fns
-        fids[[ftype]] <- out_fns
-    }
-    fids
+    bfc <- .get_cache()
+    fls <- unlist(GSE99249_files)
+    fl_paths <- .add_files(fls, paste0(baseURL, fls), bfc, verbose)
+    fl_paths <- utils::relist(fl_paths, GSE99249_files)
+    fl_paths
 }
 
 #' Download NA12878 BAM files and related data
 #'
-#' @description This function will download ~ 5 GB of data.
-#' @param path path to directory to download data
+#' @description This function will download ~100MB of data and store files in a
+#' BiocFileCache.
+#' @param verbose report messages
 #'
 #' @returns A named list with paths to an RNA-seq and WGS BAM file, and a FASTA file
 #' from hg38 chromosome 4.
@@ -65,13 +55,11 @@ download_GSE99249 <- function(path) {
 #'
 #' @examples
 #' \donttest{
-#' td <- tempdir()
-#' download_NA12878(td)
+#' download_NA12878()
 #' }
 #'
 #' @export
-download_NA12878 <- function(path) {
-    dir.create(path, recursive = TRUE, showWarnings = FALSE)
+download_NA12878 <- function(verbose = TRUE) {
 
     baseURL <- "https://raer-test-data.s3.us-west-2.amazonaws.com/NA12878/"
 
@@ -87,30 +75,18 @@ download_NA12878 <- function(path) {
         rmsk = "rmsk_hg38.tsv.gz"
     )
 
-    options(timeout = max(5000, getOption("timeout")))
-
-    fids <- list()
-    for (i in seq_along(NA12878_files)) {
-        fns <- NA12878_files[[i]]
-        ftype <- names(NA12878_files)[i]
-        out_fns <- unlist(lapply(fns, function(x) {
-            fn <- file.path(path, x)
-            if (!file.exists(fn)) {
-                # wb  necessary to avoid windows mangling line endings...
-                download.file(paste0(baseURL, x), fn, mode = "wb")
-            }
-            fn
-        }))
-        names(out_fns) <- fns
-        fids[[ftype]] <- out_fns
-    }
-    fids
+    bfc <- .get_cache()
+    fls <- unlist(NA12878_files)
+    fl_paths <- .add_files(fls, paste0(baseURL, fls), bfc, verbose)
+    fl_paths <- utils::relist(fl_paths, NA12878_files)
+    fl_paths
 }
 
 #' Download 10x PMBC bam file and related data
 #'
-#' @description This function will download < 1 GB of data.
-#' @param path path to directory to download data
+#' @description This function will download < 1 GB of data and store files in a
+#' BiocFileCache.
+#' @param verbose report messages
 #'
 #' @returns A named list with paths to bam file, fasta file,
 #' bed file of editing_sites, and an .rds file with a
@@ -120,14 +96,11 @@ download_NA12878 <- function(path) {
 #'
 #' @examples
 #' \donttest{
-#' td <- tempdir()
-#' download_human_pbmc(td)
+#' download_human_pbmc()
 #' }
 #'
 #' @export
-download_human_pbmc <- function(path) {
-    dir.create(path, recursive = TRUE, showWarnings = FALSE)
-
+download_human_pbmc <- function(verbose = TRUE) {
     baseURL <- "https://raer-test-data.s3.us-west-2.amazonaws.com/10x_human_pbmc/"
 
     pbmc_files <- list(
@@ -137,22 +110,45 @@ download_human_pbmc <- function(path) {
         sce = "sce.rds"
     )
 
-    options(timeout = max(5000, getOption("timeout")))
+    bfc <- .get_cache()
+    fls <- unlist(pbmc_files)
+    fl_paths <- .add_files(fls, paste0(baseURL, fls), bfc, verbose)
+    fl_paths <- utils::relist(fl_paths, pbmc_files)
+    fl_paths
+}
 
-    fids <- list()
-    for (i in seq_along(pbmc_files)) {
-        fns <- pbmc_files[[i]]
-        ftype <- names(pbmc_files)[i]
-        out_fns <- unlist(lapply(fns, function(x) {
-            fn <- file.path(path, x)
-            if (!file.exists(fn)) {
-                # wb necessary to avoid windows mangling line endings...
-                download.file(paste0(baseURL, x), fn, mode = "wb")
-            }
-            fn
-        }))
-        names(out_fns) <- fns
-        fids[[ftype]] <- out_fns
+#' @importFrom BiocFileCache bfcquery bfcadd bfcneedsupdate bfcdownload bfcrpath
+.add_files <- function(fls, urls, bfc, verbose = TRUE) {
+  bam_suffixes <- c(".bai", ".bam")
+  stopifnot((length(fls) == length(urls)) && (length(fls) > 0))
+
+  fls_paths <- unlist(lapply(seq_along(fls), function(i) {
+    fl <- fls[i]
+    fl_url <- urls[i]
+    is_bam <- any(vapply(bam_suffixes, endsWith, x = fl, logical(1)))
+    rid <- BiocFileCache::bfcquery(bfc, fl, "rname")$rid
+    if (!length(rid)) {
+        if(verbose) {
+            cli::cli_alert_info("Downloading file: {fl}")
+        }
+        rid <- names(BiocFileCache::bfcadd(bfc,
+                                           fl,
+                                           fpath = fl_url,
+                                           fname = ifelse(is_bam,
+                                                          "exact",
+                                                          "unique")))
     }
-    fids
+    if (!isFALSE(any(BiocFileCache::bfcneedsupdate(bfc, rid)))){
+      BiocFileCache::bfcdownload(bfc, rid)
+    }
+    BiocFileCache::bfcrpath(bfc, rids = rid[1])
+  }))
+  fls_paths
+}
+
+#' @importFrom BiocFileCache BiocFileCache
+#' @importFrom tools R_user_dir
+.get_cache <- function() {
+    cache <- tools::R_user_dir("raer", which = "cache")
+    BiocFileCache::BiocFileCache(cache)
 }
